707ff1b5a73542afbf060d3c62db84be2ad11788
LightThe <gallotheo@icloud.com>
2026-06-23 01:10:38 -0300

    feat: refactoring do service de cliente para o local correto

--- projeto-help-channel/src/services/ticketService.js
+++ projeto-help-channel/src/services/ticketService.js
@@ -1,29 +1,45 @@
+// FIXME: placeholder, precisa desenvolver toda essa conexão
+
 async function listar() {
-    // FIXME: placeholder
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-    return [
-        {
-          titulo: "Problema de rede",
-          status: "cancelado",
-          data: "25/10/1913",
-          descricao:
-            "Início do Texto Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
-        },
-        {
-          titulo: "Não consigo acessar",
-          status: "concluido",
-          data: "25/10/1913",
-          descricao:
-            "Início do Texto Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
-        },
-        {
-          titulo: "Instalação de software",
-          status: "aberto",
-          data: "25/10/1913",
-          descricao:
-            "Início do Texto Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
-        },
-      ];
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  return [
+    {
+      titulo: "Problema de rede",
+      status: "cancelado",
+      data: "25/10/1913",
+      descricao:
+        "Início do Texto Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+      usuario: "vhj8rfe9wq0",
+    },
+    {
+      titulo: "Não consigo acessar",
+      status: "concluido",
+      data: "25/10/1913",
+      descricao:
+        "Início do Texto Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+      usuario: "vhj8rfe9wq0",
+    },
+    {
+      titulo: "Instalação de software",
+      status: "aberto",
+      data: "25/10/1913",
+      descricao:
+        "Início do Texto Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+      usuario: "vhj8rfe9wq0",
+    },
+  ];
+}
+async function listarPorUsuario(idUsuario) {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  return [{
+      titulo: "Instalação de software",
+      status: "aberto",
+      data: "25/10/1913",
+      descricao:
+        "Início do Texto Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+      usuario: idUsuario,
+    },
+  ]
 }
 
-export {listar}
+export { listar, listarPorUsuario };
