821c358a43ba88106a903c0a2d265f63f0b612dc
LightThe <gallotheo@icloud.com>
2026-06-23 01:15:11 -0300

    Feat: placeholder para a tela de cadastro. Firulas visuais

--- projeto-help-channel/src/App.jsx
+++ projeto-help-channel/src/App.jsx
@@ -1,12 +1,16 @@
 import AppLayout from "./components/AppLayout";
+import CadastrarTicket from "./pages/CadastrarTicket";
+import Home from "./pages/Home";
 import ListagemTickets from "./pages/ListagemTickets";
 import { Route, Routes } from "react-router";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<AppLayout />}>
+      <Route index element={<Home />} />
+      <Route path="/tickets" element={<AppLayout />}>
         <Route index element={<ListagemTickets />} />
+        <Route path="novo" element={<CadastrarTicket />} />
       </Route>
     </Routes>
   );
